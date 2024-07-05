ed5eb5ef39a229ba4e512bf38fa6609382804576
Max Bretschneider <maxbretschneider@mailbox.org>
2024-07-05 22:01:17 +0200

    isPrime

--- pset1.hs
+++ pset1.hs
@@ -27,3 +27,14 @@ root e r
 	| pow3 half e <= r 	= searchRoot half high
 	| otherwise		= searchRoot low half
 	where half = (low + high) 'div' 2
+
+isPrime n 
+	| n < 2		= error "Zu kleine Zahl" 
+	| otherwise	= nHasNoDivisorGreaterThan 2
+	where
+		upto = root 2 n
+		nHasNoDivisorGreaterThan k 
+			| k > upto	= True
+			| n 'mod' k == 0= False
+			| otherwise	= nHasNoDivisorGreaterThan (k + 1)
+
