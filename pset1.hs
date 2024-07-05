c14508e354fea987172a761a673b26e3cedfeb8d
Max Bretschneider <maxbretschneider@mailbox.org>
2024-07-05 17:51:21 +0200

    pow2

--- pset1.hs
+++ pset1.hs
@@ -4,4 +4,7 @@ pow1 b e
  | otherwise = b * pow b (e - 1)
 
 pow2 b e
-
+ | e < 0	= error "Negative Exponent" 
+ | e == 0	= 1
+ | e 'mod' 2 == 0 = pow2 (b * b) (e 'div' 2)
+ | otherwise	= b * pow2 (b * b) (e 'div' 2)
